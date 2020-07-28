93ee5fbe13a4e760086a06504429debf5599089e
yujo <yujo@student.42seoul.kr>
2020-07-28 22:08:49 +0900

    study Hooks

---
+++ react-handling/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+import Counter from "./Hooks/Counter";
+import Info from "./Hooks/Info";
+
+function App() {
+  return (
+    <React.Fragment>
+      {/* <Counter /> */}
+      <Info />
+    </React.Fragment>
+  );
+}
+
+export default App;
