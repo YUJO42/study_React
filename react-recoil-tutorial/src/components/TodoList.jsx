6060bcdc251e4c276eb6bc3cad03911a5df15865
yujo <bedro27@gmail.com>
2021-06-13 11:40:18 +0900

    feat: recoil selector tutorial

--- react-recoil-tutorial/src/components/TodoList.jsx
+++ react-recoil-tutorial/src/components/TodoList.jsx
@@ -1,16 +1,19 @@
 import React from "react";
 import { useRecoilValue } from "recoil";
-import { todoListState } from "../atoms.js";
+import { filteredTodoListState } from "../selectors.js";
 import { TodoItemCreator } from "./TodoItemCreator.jsx";
+import TodoListFilters from "./TodoListFilters.jsx";
 import { TodoListItem } from "./TodoListItem.jsx";
+import TodoListStats from "./TodoListStats";
 
 const TodoList = () => {
-  const todoList = useRecoilValue(todoListState);
+  const todoList = useRecoilValue(filteredTodoListState);
 
   return (
     <>
+      <TodoListStats />
+      <TodoListFilters />
       <TodoItemCreator />
-
       {todoList.map((todoItem) => (
         <TodoListItem key={todoItem.id} item={todoItem} />
       ))}
